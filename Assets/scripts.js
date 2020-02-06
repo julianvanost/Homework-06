17c7eced225e3e6422be2bbd33b281350d941220
Julian <Van Ost>
2020-02-06 08:30:02 -0800

    Adds some forcast data logic

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -6,180 +6,114 @@ let thing = {
     "lat": 34.05,
     "lon": -118.24,
     "tz_id": "America/Los_Angeles",
-    "localtime_epoch": 1580924415,
-    "localtime": "2020-02-05 9:40"
+    "localtime_epoch": 1581005699,
+    "localtime": "2020-02-06 8:14"
   },
   "current": {
-    "last_updated_epoch": 1580923808,
-    "last_updated": "2020-02-05 09:30",
-    "temp_c": 12.2,
-    "temp_f": 54.0,
-    "is_day": 1,
+    "last_updated": "2020-02-06 08:15",
+    "temp_c": 11.1,
+    "temp_f": 52.0,
     "condition": {
       "text": "Sunny",
       "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
       "code": 1000
     },
     "wind_mph": 0.0,
-    "wind_kph": 0.0,
-    "wind_degree": 0,
-    "wind_dir": "N",
-    "pressure_mb": 1023.0,
-    "pressure_in": 30.7,
-    "precip_mm": 0.0,
-    "precip_in": 0.0,
-    "humidity": 18,
-    "cloud": 0,
-    "feelslike_c": 12.2,
-    "feelslike_f": 54.0,
-    "vis_km": 16.0,
-    "vis_miles": 9.0,
-    "uv": 4.0,
-    "gust_mph": 6.5,
-    "gust_kph": 10.4
+    "humidity": 44,
+    "feelslike_c": 11.1,
+    "uv": 4.0
   },
   "forecast": {
     "forecastday": [
       {
-        "date": "2020-02-05",
-        "day": {
-          "maxtemp_f": 60.8,
-          "mintemp_f": 50.4,
-          "avgtemp_f": 55.2,
-          "maxwind_mph": 8.5,
-          "totalprecip_in": 0.0,
-          "avgvis_miles": 6.0,
-          "avghumidity": 21.0,
-          "condition": {
-            "text": "Partly cloudy",
-            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
-            "code": 1003
-          },
-          "uv": 3.2
-        },
-        "astro": {
-          "sunrise": "06:47 AM",
-          "sunset": "05:27 PM",
-          "moonrise": "01:52 PM",
-          "moonset": "03:39 AM"
-        }
-      },
-      {
         "date": "2020-02-06",
         "day": {
-          "maxtemp_f": 64.6,
-          "mintemp_f": 53.8,
-          "avgtemp_f": 59.1,
-          "maxwind_mph": 7.6,
-          "totalprecip_in": 0.0,
-          "avgvis_miles": 6.0,
-          "avghumidity": 40.0,
+          "maxtemp_f": 64.2,
+          "mintemp_f": 55.6,
           "condition": {
             "text": "Partly cloudy",
             "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
             "code": 1003
           },
-          "uv": 4.1
+          "uv": 4.2
         },
         "astro": {
-          "sunrise": "06:46 AM",
-          "sunset": "05:28 PM",
-          "moonrise": "02:49 PM",
-          "moonset": "04:39 AM"
+
         }
       },
       {
         "date": "2020-02-07",
         "day": {
-          "maxtemp_f": 68.2,
-          "mintemp_f": 52.7,
-          "avgtemp_f": 60.8,
-          "maxwind_mph": 8.1,
-          "totalprecip_in": 0.0,
-          "avgvis_miles": 6.0,
-          "avghumidity": 49.0,
+          "maxtemp_f": 68.0,
+          "mintemp_f": 57.4,
           "condition": {
             "text": "Sunny",
             "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
             "code": 1000
           },
-          "uv": 4.4
+          "uv": 4.3
         },
         "astro": {
-          "sunrise": "06:45 AM",
-          "sunset": "05:29 PM",
-          "moonrise": "03:54 PM",
-          "moonset": "05:37 AM"
+
         }
       },
       {
         "date": "2020-02-08",
         "day": {
-          "maxtemp_f": 68.7,
-          "mintemp_f": 58.5,
-          "avgtemp_f": 62.5,
-          "maxwind_mph": 8.5,
-          "totalprecip_in": 0.0,
-          "avgvis_miles": 6.0,
-          "avghumidity": 52.0,
+          "maxtemp_f": 69.3,
+          "mintemp_f": 59.4,
           "condition": {
             "text": "Partly cloudy",
             "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
             "code": 1003
           },
-          "uv": 4.3
+          "uv": 4.2
         },
         "astro": {
-          "sunrise": "06:45 AM",
-          "sunset": "05:30 PM",
-          "moonrise": "05:03 PM",
-          "moonset": "06:29 AM"
+
         }
       },
       {
         "date": "2020-02-09",
         "day": {
-          "maxtemp_f": 57.7,
-          "mintemp_f": 51.8,
-          "avgtemp_f": 55.9,
-          "maxwind_mph": 13.0,
-          "totalprecip_in": 0.2,
-          "avgvis_miles": 5.0,
-          "avghumidity": 70.0,
+          "maxtemp_f": 61.3,
+          "mintemp_f": 39.6,
           "condition": {
-            "text": "Moderate or heavy rain shower",
-            "icon": "//cdn.weatherapi.com/weather/64x64/day/356.png",
-            "code": 1243
+            "text": "Patchy rain possible",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/176.png",
+            "code": 1063
           },
-          "uv": 3.0
+          "uv": 2.5
         },
         "astro": {
-          "sunrise": "06:44 AM",
-          "sunset": "05:31 PM",
-          "moonrise": "06:15 PM",
-          "moonset": "07:16 AM"
+
+        }
+      },
+      {
+        "date": "2020-02-10",
+        "day": {
+          "maxtemp_f": 68.7,
+          "mintemp_f": 52.5,
+          "condition": {
+            "text": "Partly cloudy",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+            "code": 1003
+          },
+          "uv": 5.0
+        },
+        "astro": {
+
         }
       }
     ]
   },
   "alert": {
-    "headline": "Freeze Warning issued February 05 at 1:23AM PST until February 05 at 9:00AM PST by NWS",
-    "msgtype": "Alert",
-    "severity": "Moderate",
-    "urgency": "Expected",
-    "areas": "Santa Monica Mountains Recreational Area",
-    "category": "Met",
-    "certainty": "Likely",
-    "event": "Freeze Warning",
-    "note": "Alert for Santa Monica Mountains Recreational Area (California) Issued by the National Weather Service",
-    "effective": "2020-02-05T01:23:00-08:00",
-    "expires": "2020-02-05T09:00:00-08:00",
-    "desc": "...FREEZE WARNING REMAINS IN EFFECT UNTIL 9 AM PST THIS MORNING...\n...WIND ADVISORY REMAINS IN EFFECT UNTIL 9 AM PST THIS MORNING...\n* WHAT...For the Freeze Warning, sub-freezing temperatures as\nlow as 28 expected. For the Wind Advisory, north to northeast\nwinds 15 to 30 mph with gusts up to 45 mph.\n* WHERE...Santa Monica Mountains.\n* WHEN...Until 9 AM PST this morning.\n* IMPACTS...Gusty winds will blow around unsecured objects and\nmake driving difficult, especially for high profile vehicles.\nTree limbs could be blown down and a few power outages may\nresult. Frost and freeze conditions will kill crops and other\nsensitive vegetation. Extended exposure to cold can cause\nhypothermia for animals and people. Vehicle windshields will be\nfrosted.",
-    "instruction": "Use extra caution when driving, especially if operating a high\nprofile vehicle. Secure outdoor objects.\nTake steps now to protect tender plants from the cold. Plan for\nextra time to defrost vehicle windshields."
+
   }
 }
+
 $(document).ready(function () {
-  let object = {}
+  let location = {}
   $("#makeApiCall").on('click', function (event) {
     event.preventDefault()
     let inputCity = $("#inputCityText").val();
@@ -191,7 +125,8 @@ $(document).ready(function () {
         console.log(current.uv)
         console.log(location.name)
         console.log(forecast)
-        console.log(forecast.forecastday[0])
+        // console.log(forecast.forecastday.date[0])
+        // console.log(forecast.forecastday.day[0])
 
         $('#searchResult').html(`
           <div class="card">
@@ -206,17 +141,36 @@ $(document).ready(function () {
               <h5>Wind Speed: ${current.wind_mph} Mph</h5>
             </div>
             <div class="card-action">
-              <button class="btn waves-effect waves-light addToCityCards">Add To Recent</button>
+               <!--<button class="btn waves-effect waves-light addToCityCards">Add To Recent</button>-->
             </div>
           </div>
+         <div>
+            
+
+
           `)
         $('#inputCityText').val('')
       })
       .catch(e => console.error(e))
-
+    // < div class="container" >
+    //   <h4>5 Day Forecast:</h4>
+    //   <div class="row">
+    //     <div class="col-sm-2 p-1">
+    //       <div class="card">
+    //         <div class="card-body">
+    //           <h4 id="" class="card-title">${forecast.forecastday.date}</h4>
+    //           <div class="card-image">
+    //             <img src="http:${forecast.condition.icon}" alt="${location.name}">
+    //             </div>
+    //             <p id="">maxtemp_f</p>
+    //             <p id="">mintemp_f</p>
+    //             <p id="humiditiy"></p>
+    //           </div>
+    //         </div>
+    //       </div>
     // Store to localStorage
 
-    localStorage.setItem('object', JSON.stringify(object));
+    localStorage.setItem('object', location.city);
     let list = JSON.parse(localStorage.getItem(location.name)) || []
   })
   $(document).on('click', event => {
@@ -225,14 +179,14 @@ $(document).ready(function () {
       let locationElement = document.createElement('div')
       locationElement.className = 'card'
       locationElement.innerHTML = `
-      <div id="forcastCards" class="col m4">  
-        <div class="align-left card-content">
+      <div id="forcastCards" class="col s12 align-left">Recent
+      <div class="align-left card-content">
           <h4>${location.name}</h4>
         </div>
         <div class="card-action">
           <button class="btn waves-effect waves-light removeWatchlist">Remove from Watchlist</button>
         </div>
-      </div>
+        </div>
         `
       $('#watchlist').append(locationElement)
     } else if (event.target.classList.contains('removeWatchlist')) {
@@ -240,75 +194,3 @@ $(document).ready(function () {
     }
   })
 })
-  // let location = {}
-  // let current = {}
-  // $('makeApiCall').on('click', function (event) {
-  //   event.preventDefault()
-  //   $.getJSON("http://api.weatherapi.com/v1/current.json?key=ff4f207a8ae5485cb4320106200402&q=" + inputCity)
-  //     .then(r => r.json())
-  //     .then(({
-  //       name,
-  //       region,
-  //       country,
-  //       lat,
-  //       lon,
-  //       tz_id,
-  //       localtime_epoch,
-  //       localtime,
-  //     }) => {
-  //       location = {
-  //         name: name,
-  //         region: region,
-  //         lat: lat,
-  //         lon: lon,
-  //         tz_id: tz_id,
-  //         localtime_epoch: localtime_epoch,
-  //         localtime: localtime,
-  //       }
-  //       $('movie').innerHTML = `
-  //           <div class="card">
-  //             <div class="card-image">
-  //               <img src="${Poster}" alt="${name}">
-  //               <span class="card-name">${name}</span>
-  //             </div>
-  //             <div class="card-content">
-  //               <h4>Directed by ${Director}</h4>
-  //               <h5>region: ${region}</h5>
-  //               <p>${Plot}</p>
-  //             </div>
-  //             <div class="card-action">
-  //               <button class="btn waves-effect waves-light addToCityCards">Add To Watchlist</button>
-  //             </div>
-  //           </div>
-  //           `
-  //       $('title').value = ''
-  //     })
-  //     .catch(e => console.error(e))
-
-  //   // Store to localStorage
-  //   localStorage.setItem('movie', JSON.stringify(movie));
-  //   let list = JSON.parse(localStorage.getItem(location.name)) || []
-  // })
-
-  // document.on('click', event => {
-  //   if (event.target.classList.contains('addToCityCards')) {
-  //     event.target.parentNode.parentNode.remove()
-  //     let locationElement = document.createElement('div')
-  //     locationElement.className = 'card'
-  //     locationElement.innerHTML = `
-  //         <div class="card-content">
-  //           <h3>${location.name}</h3>
-  //           <h4>Directed by ${movie.director}</h4>
-  //           <h5>Year: ${movie.year}</h5>
-  //           <p>${movie.plot}</p>
-  //         </div>
-  //         <div class="card-action">
-  //           <button class="btn waves-effect waves-light removeWatchlist">Remove from Watchlist</button>
-  //         </div>
-  //         `
-  //     $('watchlist').append(locationElement)
-  //   } else if (event.target.classList.contains('removeWatchlist')) {
-  //   }
-  //     event.target.parentNode.parentNode.remove()
-  // })
-// })
